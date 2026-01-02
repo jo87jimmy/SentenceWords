3b4655f84fad6bedb00b4daaccccba74d567559b
jo87jimmy <jo87jimmy@gmail.com>
2026-01-02 14:40:13 +0800

    feat: 新增 Pinia 運行時與設定狀態管理，並實作單字練習頁面與全域輔助功能。

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,10 +1,67 @@
+import dayjs from 'dayjs'
+import { getDefaultSettingState } from "@/stores/setting.ts";
+
 export function cloneDeep<T>(val: T) {
     return JSON.parse(JSON.stringify(val))
 }
+//获取完成天数
+export function _getAccomplishDays(total: number, dayNumber: number) {
+    return Math.ceil(total / dayNumber)
+}
+
+//获取完成日期
+export function _getAccomplishDate(total: number, dayNumber: number) {
+    if (dayNumber <= 0) return '-'
+    let d = _getAccomplishDays(total, dayNumber)
+    return dayjs().add(d, 'day').format('YYYY-MM-DD')
+}
+
 //获取学习进度
 export function _getStudyProgress(index: number, total: number) {
     return Number(((index / total) * 100).toFixed())
 }
+
+export function checkAndUpgradeSaveSetting(val: any) {
+    // console.log(configStr)
+    // console.log('s', new Blob([val]).size)
+    // val = ''
+    let defaultState = getDefaultSettingState()
+    if (val) {
+        try {
+            let data
+            if (typeof val === 'string') {
+                data = JSON.parse(val)
+            } else {
+                data = val
+            }
+            if (!data.version) return defaultState
+            let state: SettingState & { [key: string]: any } = data.val
+            if (typeof state !== 'object') return defaultState
+            state.load = false
+            let version = Number(data.version)
+            if (version === SAVE_SETTING_KEY.version) {
+                checkRiskKey(defaultState.shortcutKeyMap, state.shortcutKeyMap)
+                delete state.shortcutKeyMap
+                checkRiskKey(defaultState, state)
+                return defaultState
+            } else {
+                if (version === 13) {
+                    defaultState.soundType = state.soundType
+                }
+                //为了保持永远是最新的快捷键选项列表，但保留住用户的自定义设置，去掉无效的快捷键选项
+                //例: 2版本，可能有快捷键A。3版本没有了
+                checkRiskKey(defaultState.shortcutKeyMap, state.shortcutKeyMap)
+                delete state.shortcutKeyMap
+                checkRiskKey(defaultState, state)
+                return defaultState
+            }
+        } catch (e) {
+            return defaultState
+        }
+    }
+    return defaultState
+}
+
 export function checkAndUpgradeSaveDict(val: any) {
     // console.log(configStr)
     // console.log('s', new Blob([val]).size)
@@ -60,4 +117,18 @@ export function shakeCommonDict(n: BaseState): BaseState {
         }
     })
     return data
+}
+
+export function useNav() {
+    const router = useRouter()
+    const runtimeStore = useRuntimeStore()
+
+    function nav(path, query = {}, data?: any) {
+        if (data) {
+            runtimeStore.routeData = cloneDeep(data)
+        }
+        router.push({ path, query })
+    }
+
+    return { nav, push: nav, back: router.back }
 }
